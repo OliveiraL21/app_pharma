5b16a28e651e86724624b31ff70bde84e343fd0d
Lucas Oliveira <lucasoliveira508@gmail.com>
2021-08-09 19:58:57 -0300

    Criando Classe Produto e Corrigindo a classe Farmacia

---
+++ lib/Model/Farmacia.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Farmacia {
+  String _Nome = "";
+  String _Cnpj = "";
+  String _Endereco = "";
+  String _Telefone = "";
+  String _HorarioDeFuncionamento = "";
+  int _NumeroDeLicencaSanitaria = 0;
+  int _NumeroDeAutorizacaoDeFuncionamento = 0;
+}
+
---
+++ lib/Model/Produtos.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ class Produto {
+  String _Nome = "";
+  String PrincipioAtivo = "";
+  String _Apresentacao = "";
+  String _FormaFarmaceutica = "";
+  int _Quantidade = 0;
+  int _NumeroDeRegistroAnvisa = 0;
+  String _Marca = "";
+  double _Preco = 0.00;
+
+}

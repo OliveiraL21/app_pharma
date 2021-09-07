acf0d57e443768d41c8860bca3772a0e6595ef01
Lucas Oliveira <lucasoliveira508@gmail.com>
2021-09-07 00:32:42 -0300

    criando os radio buttons da page intermediaria

--- lib/views/PageChoseCadastro.dart
+++ lib/views/PageChoseCadastro.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:cpf_cnpj_validator/cpf_validator.dart';
+import 'package:pharma_app/views/PageCadastroFarmacia.dart';
+
+import 'PageCadastro.dart';
 
 class pageChooseCadastro extends StatefulWidget {
   const pageChooseCadastro({Key? key}) : super(key: key);
@@ -12,6 +15,7 @@ class pageChooseCadastro extends StatefulWidget {
 
 class _pageChooseCadastroState extends State<pageChooseCadastro> {
 
+  String? _opcao = "";
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -75,7 +79,87 @@ class _pageChooseCadastroState extends State<pageChooseCadastro> {
                             ),
                         ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RadioListTile(
+                                title: Text("Farmácia"),
+                                subtitle: Text("Escolha uma opção"),
+                                value: "Farmacia",
+                                groupValue: _opcao,
+                                onChanged: (String? valor){
+                                  setState((){
+                                    _opcao = valor;
+                                  });
+                                },
+                              ),
 
+                              RadioListTile(
+                                title: Text("Usuário"),
+                                subtitle: Text("Escolha uma opção"),
+                                value: "Usuario",
+                                groupValue: _opcao,
+                                onChanged: (String? valor){
+                                  setState((){
+                                    _opcao = valor;
+                                  });
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 300,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            if(_opcao == "Farmacia"){
+
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => pageCadastroFarmacia()),
+                              );
+                            }
+                            else{
+
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => pageCadastro()),
+                              );
+                            }
+                          },
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              side: BorderSide(
+                                width: 1.0,
+                                color: Color.fromARGB(255, 49, 175, 180),
+                              )),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Cadastrar',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 49, 175, 180),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                              Icon(
+                                Icons.article_outlined,
+                                color: Color.fromARGB(255, 49, 175, 180),
+                                size: 30,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                       ElevatedButton(
                         onPressed: () {
                           Navigator.pop(context);
